882b7e2d3fe443249aac8fbc1ffbb2486c51bd0d
Erik Steigler <erik@lot18.com>
2012-04-11 17:49:39 -0400

    pep8 whitespace updates

--- holdem.py
+++ holdem.py
@@ -1,20 +1,21 @@
 # Le holdem game
 
-import string 
-import sys 
+import string
+import sys
 import random
 import threading
 from time import *
 from settings import *
 
+
 class Holdem(threading.Thread):
-    
+
     def __init__(self,mongo):
         threading.Thread.__init__(self)
         self.mongo = mongo
-        
+
     def run(self):
-        
+
         self.stake = self.mongo.values.pop(0)
         self.blind = 1
         self.order = self.mongo.values
@@ -23,22 +24,22 @@ class Holdem(threading.Thread):
         self.players = {}
         self.turn = False # set to player
 
-        random.shuffle(self.order)  
-        
+        random.shuffle(self.order)
+
         for player in self.order:
-            self.players[player] = { 
+            self.players[player] = {
                 "money":int(self.stake),
                 "hand":[],
                 "stake":0,
                 "status":"in", # in,folded,sitout,waiting
             }
-        
-        
+
+
         # self.suits = ['s','h','d','c']
         self.suits = [u'\u2660',u'\u2661',u'\u2662',u'\u2663']
         # find with self.ordinal.index[what]
         self.ordinal = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-        self.cards = [] 
+        self.cards = []
 
         for suit in self.suits:
             for ordinal in self.ordinal:
@@ -49,7 +50,7 @@ class Holdem(threading.Thread):
         while True:
             # process game state
             continue
-        
+
     # player actions
 
     def bet(self):
@@ -69,18 +70,18 @@ class Holdem(threading.Thread):
 
     def allin(self):
         return
-        
+
     def setup(self):
         # enter blinds
         # increment turn
-        # set dealer 
+        # set dealer
         # pop player and append or something
         # clear hands
         return
 
     def turn(self,jump = 0):
         return
-        
+
     def deal(self):
         self.cardpointer = 0
         random.shuffle(self.cards)
