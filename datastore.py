3a5b0757334014c58a05d7c558b7252a2f5a86ad
Hunt Welch <hunt@huntwelch.com>
2014-03-06 00:36:48 +0000

    changes

--- datastore.py
+++ datastore.py
@@ -73,6 +73,9 @@ class Position(mongoengine.EmbeddedDocument):
     quantity = IntField(min_value=0)
     type = StringField()
 
+class Alias(mongoengine.EmbeddedDocument):
+    name = StringField(required=True)
+    definition = StringField(required=True)
 
 class Drinker(mongoengine.Document):
     name = StringField(required=True)
@@ -83,14 +86,13 @@ class Drinker(mongoengine.Document):
     awaiting = StringField()
     cash = FloatField(default=STARTING_CASH)
     positions = ListField(EmbeddedDocumentField(Position))
-
+    aliases = ListField(EmbeddedDocumentField(Alias))
 
 class Words(mongoengine.Document):
     word = StringField(required=True)
     partofspeech = StringField(required=True)
     definition = StringField(required=True)
     source = StringField(required=True)
-
 
 class Learned(mongoengine.Document):
     word = StringField(required=True)
